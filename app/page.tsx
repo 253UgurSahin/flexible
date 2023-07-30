22e26862baf98f594c6088314045eeddfb736ada
253ugursahin <ugurs156@gmail.com>
2023-07-30 22:06:06 +0200

    push it

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex-start flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>Load more</h1>
+        </div>
+    )
+}
---
+++ components/AuthProviders.tsx
@@ -0,0 +1,7 @@
+export default function AuthProviders() {
+    return (
+        <>
+            Authproviders
+        </>
+    )
+}
